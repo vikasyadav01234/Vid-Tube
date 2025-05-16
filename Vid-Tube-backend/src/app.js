0253fcc085a97807eb0e078bfbf256882e0e0657
vikasyadav01234 <yadav2911vikas@gmail.com>
2025-05-16 12:52:20 +0530

    fix: Update CORS origin to the correct frontend URL

--- Vid-Tube-backend/src/app.js
+++ Vid-Tube-backend/src/app.js
@@ -6,7 +6,7 @@ import morgan from 'morgan';
 const app = express();
 
 app.use(cors({
-    origin: ["https://tweet-tube-frontend.vercel.app", "http://localhost:5173"], 
+    origin: ["https://vid-tube-five.vercel.app", "http://localhost:5173"], 
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
